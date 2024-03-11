72627d10cecc1759b19a56c1389a85f1af80ac1b
Your Name <you@example.com>
2024-03-11 19:53:07 +0500

    Настройка получения данных из строки

--- index.php
+++ index.php
@@ -5,14 +5,23 @@
     <title>Тестовое задание</title>
 </head>
 <body class="full">
+    <form method="post">
+        <div>
+            <P class="data">Введите строку</P>
+            <input type="text" id="21" name="data">
+        </div>
+        <div class="globalButton">
+            <input type="submit" name="send" value="Преобразовать">
+        </div>
+    </form>
     <div>
-        <P class="">Введите строку</P>
-        <input type="text" id="21" name="callCenter">
+        <?php echo'Вы ввели : <br>';
+        if (isset($_POST['data'])) {
+            
+            print($_POST['data']);
+        }
+        ?>
     </div>
-    <div class="globalButton">
-        <input type="submit" name="send" value="Преобразовать">
-    </div>
-
     <style>
         .full{
             background-color: #fff7fc;
