7508e1cc850af981948df9af4bf0b48d93b7bfc2
Your Name <you@example.com>
2024-03-11 21:25:13 +0500

    Написал метод, который принимает на вход строку и меняет порядок букв в каждом слове на обратный с учетом регистра и пунктуации

--- index.php
+++ index.php
@@ -15,13 +15,44 @@
         </div>
     </form>
     <div>
-        <?php echo'Вы ввели : <br>';
+        <?php 
         if (isset($_POST['data'])) {
-            
+            $data = $_POST['data'];
+            echo'Вы ввели : <br>';
             print($_POST['data']);
+            echo '<br> Строка после преобразования: <br>';
+            print(splitWords($data)); // Выводим строку после преобразования 
         }
         ?>
     </div>
+
+    <?php 
+        function splitWords($str){
+            $arr = explode(' ', $str); // Отделяем слова
+            foreach ($arr as $key => $elem) {
+                $arr[$key] = reversWords($elem); // пробегаем по каждому слову и применяем функцию для изменения порядка символов 
+            }
+            return implode(' ', $arr); // Восстанавливаем строку 
+        };
+        function reversWords($word){ // функция для изменения порядка символов
+            $arr = str_split($word); // Разбиваем строку на элементы массива 
+            $arrRev = str_split(mb_strtolower(preg_replace('/\pP/iu', '', $word)));// Разбиваем строку на элементы массива и приводим их к нижнему регистру без учета знаков пунктуации  
+            $num = count( $arrRev ) -1; // Колличество элементов реверсированного массива
+
+            for ($i = 0; $i < count($arr); $i++) {
+                if(ctype_punct($arr[$i])) continue; // Если элемент массива это знак пунктуации
+                if(ctype_upper($arr[$i])){ // Если элемент массива в верхнем регистре 
+                    $arr[$i] = strtoupper($arrRev[$num]); // приводим элемент реверсированного массива к верхнему регистру и заменяем им элемент основного массива 
+                } else {
+                    $arr[$i] = $arrRev[$num]; // Заменям элемент основного массива
+                }
+                $num --;
+            }
+            return implode($arr); // Восстанавливаем строку  
+        } 
+
+    ?>
+
     <style>
         .full{
             background-color: #fff7fc;
@@ -31,4 +62,4 @@
         }
     </style>
 </body>
-</html>
+</html> 
